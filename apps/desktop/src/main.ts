30fe4cd04784d6abeb49ab05955dedfaab55e916
maxnes3 <draw_max@mail.ru>
2026-08-28 02:01:49 +0400

    feat: create desktop app

---
+++ apps/desktop/src/main.ts
@@ -0,0 +1,46 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'node:path';
+import started from 'electron-squirrel-startup';
+
+if (started) {
+  app.quit();
+}
+
+const createWindow = (): void => {
+  const mainWindow = new BrowserWindow({
+    fullscreen: true,
+    autoHideMenuBar: true,
+
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+
+      nodeIntegration: false,
+      contextIsolation: true,
+      sandbox: true,
+    },
+  });
+
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    void mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    void mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`),
+    );
+  }
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
